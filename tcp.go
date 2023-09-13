e000e6d177387eb5c32ee6ad17718faa5a52c64a
Felix Riebl <felix.riebl@prodot.de>
2023-09-13 23:22:21 +0200

    Try adding module instance and stuff

--- tcp.go
+++ tcp.go
@@ -14,6 +14,46 @@ func init() {
 	k6modules.Register("k6/x/tcpx", new(TCP))
 }
 
+type (
+	// RootModule is the global module instance that will create module
+	// instances for each VU.
+	RootModule struct{}
+
+	// ModuleInstance represents an instance of the JS module.
+	ModuleInstance struct {
+		// vu provides methods for accessing internal k6 objects for a VU
+		vu k6modules.VU
+		// comparator is the exported type
+		tcp *TCP
+	}
+)
+
+// Ensure the interfaces are implemented correctly.
+var (
+	_ k6modules.Instance = &ModuleInstance{}
+	_ k6modules.Module   = &RootModule{}
+)
+
+// New returns a pointer to a new RootModule instance.
+func New() *RootModule {
+	return &RootModule{}
+}
+
+// NewModuleInstance implements the modules.Module interface returning a new instance for each VU.
+func (*RootModule) NewModuleInstance(vu k6modules.VU) k6modules.Instance {
+	return &ModuleInstance{
+		vu:  vu,
+		tcp: &TCP{vu: vu},
+	}
+}
+
+// Exports implements the modules.Instance interface and returns the exported types for the JS module.
+func (mi *ModuleInstance) Exports() k6modules.Exports {
+	return k6modules.Exports{
+		Default: mi.tcp,
+	}
+}
+
 type TCP struct {
 	vu k6modules.VU
 }
