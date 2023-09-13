84114746f518471b303df08481df7e5f50b39024
Felix Riebl <felix.riebl@prodot.de>
2023-09-13 22:12:53 +0200

    Added data received and data sent internal metrics to extension

--- tcp.go
+++ tcp.go
@@ -2,17 +2,21 @@ package tcp
 
 import (
 	"net"
-	// "net/TCPConn"
-	"go.k6.io/k6/js/modules"
+	"time"
+
+	k6modules "go.k6.io/k6/js/modules"
+	k6metrics "go.k6.io/k6/metrics"
 	// "io"
 	// "bufio"
 )
 
 func init() {
-	modules.Register("k6/x/tcpx", new(TCP))
+	k6modules.Register("k6/x/tcpx", new(TCP))
 }
 
-type TCP struct{}
+type TCP struct {
+	vu k6modules.VU
+}
 
 func (tcp *TCP) Connect(address string) (*net.TCPConn, error) {
 	addr, err := net.ResolveTCPAddr("tcp", address)
@@ -25,41 +29,43 @@ func (tcp *TCP) Connect(address string) (*net.TCPConn, error) {
 }
 
 func (tcp *TCP) Write(conn *net.TCPConn, data []byte) error {
-	_, err := conn.Write(data)
+	writeByteCount, err := conn.Write(data)
 	if err != nil {
 		return err
 	}
+
+	state := tcp.vu.State()
+	dataSendMetric := state.BuiltinMetrics.DataSent
+	state.BuiltinMetrics.DataSent.Sink.Add(k6metrics.Sample{
+		TimeSeries: k6metrics.TimeSeries{
+			Metric: dataSendMetric,
+			Tags:   state.Tags.GetCurrentValues().Tags},
+		Value: float64(writeByteCount),
+		Time:  time.Now().UTC(),
+	})
 
 	return nil
 }
 
 func (tcp *TCP) Read(conn *net.TCPConn, size int) ([]byte, error) {
 	buf := make([]byte, size)
-	_, err := conn.Read(buf)
+	readByteCount, err := conn.Read(buf)
 	if err != nil {
 		return nil, err
 	}
+
+	state := tcp.vu.State()
+	dataReceiveMetric := state.BuiltinMetrics.DataSent
+	state.BuiltinMetrics.DataReceived.Sink.Add(k6metrics.Sample{
+		TimeSeries: k6metrics.TimeSeries{
+			Metric: dataReceiveMetric,
+			Tags:   state.Tags.GetCurrentValues().Tags},
+		Value: float64(readByteCount),
+		Time:  time.Now().UTC(),
+	})
+
 	return buf, nil
 }
-
-// func ReadLn(conn net.Conn, delim byte) (string, error) {
-// 	reader := bufio.NewReader(conn)
-// 	var buffer bytes.Buffer
-// 	for {
-// 		ba, isPrefix, err := reader.ReadLine()
-// 		if err != nil {
-// 			if err == io.EOF {
-// 				break
-// 			}
-// 			return "", err
-// 		}
-// 		buffer.Write(ba)
-// 		if !isPrefix {
-// 			break
-// 		}
-// 	}
-// 	return buffer.String(), nil
-// }
 
 func (tcp *TCP) WriteBytesLn(conn *net.TCPConn, data []byte, delim []byte) error {
 	return tcp.Write(conn, append(data, delim...))
